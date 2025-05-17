1d80803b7c2ec75bb67872c131e61ccb9019aa01
dopemmanuel <eolm728@g.educaand.es>
2025-05-17 02:11:03 +0200

    Adding debug!!

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,24 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib-jdk8"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2") // Kotest framework
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2") // Aserciones
+    testImplementation("io.kotest:kotest-property:5.6.2") // Pruebas basadas en propiedades
+    testImplementation("io.mockk:mockk:1.13.7") // Mocking library
+
+    // JUnit 5
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed", "standardOut", "standardError")
+    }
 }
 kotlin {
     jvmToolchain(21)
+    jvmToolchain(17)
     jvmToolchain(8)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+/**
 pluginManagement {
     plugins {
         kotlin("jvm") version "2.1.20"
@@ -6,5 +7,21 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "PRO_2425_u4u5u6_TaskManager"
+rootProject.name = "Debug_log_taskmanager"
+*/
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        kotlin("jvm") version "2.1.20" // Versión estable más reciente (corregido de 2.1.20)
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "Debug_log_taskmanager"
