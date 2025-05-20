f62d7064bb59d0f51a55f546bf11e423cf4f7f59
dopemmanuel <eolm728@g.educaand.es>
2025-05-20 19:52:53 +0200

    Hechoooo!!!

--- src/main/kotlin/servicios/UsuarioService.kt
+++ src/main/kotlin/servicios/UsuarioService.kt
@@ -6,7 +6,14 @@ import iesra.prog2425.modelo.Usuario
 
 class UsuarioService(private val usuarioRepository: UsuarioRepository) {
     fun registrarUsuario(nombre: String, email: String): Usuario {
-        return usuarioRepository.crearUsuario(nombre, email)
+            if (nombre.isBlank()) {
+                throw IllegalArgumentException("El nombre no puede estar vacío")
+            }
+
+            if (!email.contains("@")) {
+                throw IllegalArgumentException("Email no válido. Debe contener @")
+            }
+                return usuarioRepository.crearUsuario(nombre, email)
     }
 
     fun listarUsuarios(): List<Usuario> {
